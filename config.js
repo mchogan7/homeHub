0f6bf34cfd69dc6d95918db2e3b8737f86f00c2b
mhogan7 <mills.c.hogan@gmail.com>
2018-05-28 22:10:11 -0500

    Setting up server for websockets.

---
+++ config.js
@@ -0,0 +1,13 @@
+let config = {
+devDB: {
+  user: "postgres",
+  host: "127.0.0.1",
+  database: "postgres",
+  password: "mchez1206",
+  port: 5432
+}
+
+}
+
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 8000
-
-const { Pool, Client } = require('pg')
-
-const client = new Client({
-    user: 'postgres',
-    host: '127.0.0.1',
-    database: 'postgres',
-    password: 'mchez1206',
-    port: 5432,
-})
-client.connect()
+const config = require('./config.js');
+const express = require("express");
+const app = express();
+const port = 8000;
 
 
+const { Pool, Client } = require("pg");
 
-app.get('/', (req, res) => {
+const client = new Client(config.devDB);
+client.connect();
 
+app.get("/", (req, res) => {
+  client.query("SELECT NOW()", (err, response) => {
+    console.log(err, response);
+    res.send(response.rows[0]);
+    client.end();
+  });
+});
 
-
-    client.query('SELECT NOW()', (err, response) => {
-        console.log(err, response)
-        res.send(response.rows[0])
-        client.end()
-      })
-        
-
-})
-
-app.listen(8000, () => console.log('Listening on port ' + port))
+app.listen(port, () => console.log("Listening on port " + port));
